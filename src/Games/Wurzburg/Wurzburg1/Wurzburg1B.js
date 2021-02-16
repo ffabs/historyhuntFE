5fade2e727789592d7d96b9f6f6665e4e1045194
ffabs <fabioandferrari@gmail.com>
2021-02-16 11:12:18 +0100

    adding ciceru.com in the cname

--- src/Games/Wurzburg/Wurzburg1/Wurzburg1B.js
+++ src/Games/Wurzburg/Wurzburg1/Wurzburg1B.js
@@ -9,7 +9,7 @@ function Wurzburg1B() {
       <div className="clue-background">
         <div className="clue-background-title">A martyrdom that changed history</div>
         <div className="clue-background-div">At the end of the 7th century, three Irish missionaries brought Christianity to the region. Even if this mission got them killed, they succeed eventually.</div>
-        <div className="clue-background-div">The three saints are are still commemorated on this very bridge today, each with a statue among the 12 present here.</div>
+        <div className="clue-background-div">The three saints are still commemorated on this very bridge today, each with a statue among the 12 present here.</div>
       </div>
       <div className="question-section">
           <div className="clue-background-div">Are you ready to find them?</div>
