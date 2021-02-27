c625ff010c1ff74964ec91e56ea7d59f90ec9fba
ffabs <fabioandferrari@gmail.com>
2021-02-27 20:49:24 +0100

    clue 11 kiliani fireworks

--- src/Games/Wurzburg/Wurzburg11/Wurzburg11D.js
+++ src/Games/Wurzburg/Wurzburg11/Wurzburg11D.js
@@ -11,7 +11,7 @@ function Wurzburg11D() {
         <div className="clue-background-title">Congrats for reaching the end!</div>
         <div className="clue-background-div">This is the end of the journey. Hopefully you enjoyed discovering the history of this great city!</div>
         <img src={kilianifest} className="gameIntro-image" alt="kilianifest" />
-        <div className="clue-background-div">By the way, do you remember how the journey started? Today, Kilian still plays an important role for the city, he is the patron saint of Würzburg and always in July there is a great fair called "Kiliani"!</div>
+        <div className="clue-background-div">By the way, do you remember how the journey started? Today, Kilian still plays an important role for the city, he is the patron saint of Würzburg and always in July there is a big fair with fireworks called "Kiliani"!</div>
       </div>
       {/* <div className="question-section"> */}
       {/* </div> */}
